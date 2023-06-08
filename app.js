24435f6044a00a9c1a941c87f62d80e4372bf11d
yunwuu <zewenlee2000@outlook.com>
2023-06-08 17:23:30 +0800

    feat: 用户数据redis基本表结构 & API

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import { verifyJWT, generateJWT } from './utils/auth.js';
 import { getClient } from './utils/db.js';
-import { createClient } from 'redis';
 
 
 // 初始化
@@ -19,17 +18,21 @@ if (!process.env.dburl) throw new Error('DB url not found.');
 
 // 处理 Auth Header
 app.all("*", (req, res, next) => {
-    if (['/login'].indexOf(req.path) != -1) 
+    if (['/login', '/user/init'].indexOf(req.path) != -1) {
         next();
-    const authHeader = req.headers['Authorization'] || req.headers['authorization'];
-    if (!authHeader) return res.status(500).json({ code: -1, errmsg: 'No auth header.' });
-    const token = authHeader.split(' ')[1];
-    console.log(`Received token: ${token}`);
-    if (!token) return res.status(500).json({ code: -2, errmsg: 'Auth header is invalid.' });
-    const verify = verifyJWT(token, process.env.jwtsecret);
-    if (!verify) return res.status(401).json({ code: -3, errmsg: 'Unauthorized' });
-    req.vresult = verify;
-    next();
+    }
+    else 
+    {
+        const authHeader = req.headers['Authorization'] || req.headers['authorization'];
+        if (!authHeader) return res.status(500).json({ code: -1, errmsg: 'No auth header.' });
+        const token = authHeader.split(' ')[1];
+        console.log(`Received token: ${token}`);
+        if (!token) return res.status(500).json({ code: -2, errmsg: 'Auth header is invalid.' });
+        const verify = verifyJWT(token, process.env.jwtsecret);
+        if (!verify) return res.status(401).json({ code: -3, errmsg: 'Unauthorized' });
+        req.vresult = verify;
+        next();
+    }
 })
 
 
@@ -49,27 +52,51 @@ app.post('/login', async (req, res) => {
     const pwd = req.body.pwd;
     if (!id || !pwd) return res.status(500).json({ code: -1, errmsg: 'uid and/or password not found.' })
     // ... 查数据库 => username
-    const username = 'test'; 
-    return res.json({ code: 0, result: generateJWT({id, username}, process.env.jwtsecret) });
+    const client = getClient(process.env.dburl);
+    await client.connect();
+    const queryId = await client.hGetAll(String(id));
+    if (Object.keys(queryId).length == 0) {
+        // 不存在该ID
+        return res.status(401).json({ code: -1, errmsg: 'User not found' });
+    }
+    if (!queryId.username || !queryId.password) {
+        // 未被正确初始化
+        return res.status(500).json({ code: -1, errmsg: 'User wasn\'t initialized correctly.' });
+    }
+    if (queryId.password != pwd) {
+        // 密码不正确
+        return res.status(403).json({ code: -1, errmsg: 'Username or password incorrect.' });
+    }
+    const username = queryId.username;
+    return res.json({ code: 0, result: generateJWT({id, username}, process.env.jwtsecret) }); // 下发Token
 })
 
 
 // 初始化用户数据表
 app.get('/user/init', async (req, res) => {
-    try {
-        const uinfo = req.vresult;
-        const client = getClient(process.env.dburl);
-        // const client = createClient({
-        //     url: process.env.dburl
-        // });
-        await client.connect();
-        await client.hSet(uinfo.id, 'username', uinfo.username);
-        await client.hSet(uinfo.id, 'lastupdate', 0);
-        await client.disconnect();
-    } catch (err) {
-        return res.status(500).json({ code: -1, errmsg: err });
+    const { id, pwd, uname } = req.body;
+    if (!id || !pwd || !uname) {
+        return res.status(500).json({ code: -1, errmsg: 'No id/pwd/uname.' });
     }
-    return res.json({ code: 0, msg: 'OK' });
+    else {
+        const client = getClient(process.env.dburl);
+        await client.connect();
+        const queryId = await client.hGetAll(String(id));
+        if (Object.keys(queryId).length > 0) {
+            return res.status(500).json({ code: -1, errmsg: 'Uid has been used.' });
+        }
+        else
+        {
+            try {
+                await client.hSet(id, 'username', uname);
+                await client.hSet(id, 'password', pwd);
+                await client.hSet(id, 'lastupdate', 0);
+            } catch (err) {
+                return res.status(500).json({ code: -1, errmsg: err });
+            }
+            return res.json({ code: 0, result: 'OK' });
+        }
+    }
 })
 
 
